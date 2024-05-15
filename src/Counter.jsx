de8c9bf7366b850dab47b15757a6ad3dbbbbcda8
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-05-15 21:29:35 +0530

    Fixed Counter text

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,7 +7,7 @@ function Counter() {
   return (
     <div>
         <h1>Counter App</h1>
-        <div>Count : {count}</div>
+        <div>Count: {count}</div>
         <button onClick={e => setCount(prev => prev + 1)}>Increment</button>
         <button onClick={e => setCount(prev => prev - 1)}>Decrement</button>
     </div>
